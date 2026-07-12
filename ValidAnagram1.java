323434c47761c06b76a16587df373899b34c1b5a
iamdivye <divyejaiswal5@gmail.com>
2026-07-12 16:33:41 +0530

    https://leetcode.com/problems/valid-anagram/?envType=problem-list-v2&envId=string

---
+++ ValidAnagram1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int a[]=new int[26];
+        if(s.length() != t.length())
+        return false;
+        for(int i=0;i<s.length();i++){
+            a[s.charAt(i)-97]++;
+            a[t.charAt(i)-97]--;
+        }
+        for(int i=0;i<26;i++){
+            if(a[i] != 0)
+            return false;
+        }
+        return true;
+    }
+}
