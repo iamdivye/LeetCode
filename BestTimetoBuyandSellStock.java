bffc8875fceef7a0d8c46e8fcf6963b3c4d839d2
iamdivye <divyejaiswal5@gmail.com>
2026-09-02 22:34:02 +0530

    https://leetcode.com/problems/best-time-to-buy-and-sell-stock/

---
+++ BestTimetoBuyandSellStock.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min=prices[0];
+        int diff=0;
+        for(int i=1;i<prices.length;i++){
+            min = Math.min(min, prices[i]);
+            diff = Math.max(diff, prices[i] - min);
+        }
+        return diff;
+    }
+}
